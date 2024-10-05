3542fa8a25294709d801c9433a7d1dc7833f4d97
eddiehsu66 <927105384@qq.com>
2024-10-05 20:33:56 +0800

    add ERC1155 case

---
+++ ERC1155/WTF1155.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+import "./ERC1155.sol";
+
+contract WTF1155 is ERC1155{
+    uint256 constant MAX_ID = 1000;
+
+    constructor() ERC1155("wtf","wtf"){}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/";
+    }
+    
+    // 铸造函数
+    function mint(address to, uint256 id, uint256 amount) external {
+        // id 不能超过10,000
+        require(id < MAX_ID, "id overflow");
+        _mint(to, id, amount, "");
+    }
+
+    // 批量铸造函数
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts) external {
+        // id 不能超过10,000
+        for (uint256 i = 0; i < ids.length; i++) {
+            require(ids[i] < MAX_ID, "id overflow");
+        }
+        _mintBatch(to, ids, amounts, "");
+    }
+}
